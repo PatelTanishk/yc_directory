43020bb615ebb9dd0e7e289c86a97562f7d2b80b
TanishkPatel03 <pateltanishk03@gmail.com>
2025-06-12 23:27:29 +0530

    yc_directory

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,10 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
-import {auth} from "@/auth";
 
 export default async function Home({searchParams} : {searchParams: Promise<{query?:string}>}) {
     const query = (await searchParams).query;
     const params = {search:query || null}
-    const session = await auth();
     const {data:posts} = await sanityFetch({query:STARTUPS_QUERY, params})
     // const posts = [
     //     {
